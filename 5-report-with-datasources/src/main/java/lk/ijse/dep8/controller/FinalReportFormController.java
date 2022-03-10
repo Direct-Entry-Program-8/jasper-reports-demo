97fd03c135bfb3bea336e15d8dd6c4a2c39ab722
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-03-10 15:28:28 +0530

    Add JRViewer to JavaFX

--- 5-report-with-datasources/src/main/java/lk/ijse/dep8/controller/FinalReportFormController.java
+++ 5-report-with-datasources/src/main/java/lk/ijse/dep8/controller/FinalReportFormController.java
@@ -1,22 +1,31 @@
 package lk.ijse.dep8.controller;
 
+import javafx.embed.swing.SwingNode;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import lk.ijse.dep8.db.DBConnection;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.design.JasperDesign;
 import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.swing.JRViewer;
 import net.sf.jasperreports.view.JasperViewer;
 
+import javax.swing.*;
 import java.sql.Connection;
 import java.util.HashMap;
 
 public class FinalReportFormController {
     public TextField txtInput;
+    public AnchorPane pneContainer;
     private Connection connection;
+    private SwingNode swingNode;
 
     public void initialize(){
         connection = DBConnection.getInstance().getConnection();
+        swingNode = new SwingNode();
+        pneContainer.getChildren().add(swingNode);
     }
 
     public void btnSearch_OnAction(ActionEvent actionEvent) {
@@ -28,8 +37,14 @@ public class FinalReportFormController {
             HashMap<String, Object> parameters = new HashMap<>();
             parameters.put("query", txtInput.getText());
             JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, connection);
+            //JasperViewer.viewReport(jasperPrint, false);
+            JRViewer viewer = new JRViewer(jasperPrint);
+            SwingUtilities.invokeLater(()->{
+                swingNode.setContent(viewer);
+            });
 
-            JasperViewer.viewReport(jasperPrint, false);
+            txtInput.requestFocus();
+            txtInput.selectAll();
         } catch (JRException e) {
             e.printStackTrace();
         }
