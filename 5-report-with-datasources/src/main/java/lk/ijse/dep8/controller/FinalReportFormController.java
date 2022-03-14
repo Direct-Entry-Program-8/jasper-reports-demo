ca87f3dfc9f676cafefeedc70505795b1de7446a
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-03-14 17:01:01 +0530

    Compiled reports

--- 5-report-with-datasources/src/main/java/lk/ijse/dep8/controller/FinalReportFormController.java
+++ 5-report-with-datasources/src/main/java/lk/ijse/dep8/controller/FinalReportFormController.java
@@ -2,17 +2,19 @@ package lk.ijse.dep8.controller;
 
 import javafx.embed.swing.SwingNode;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import lk.ijse.dep8.db.DBConnection;
-import net.sf.jasperreports.engine.*;
-import net.sf.jasperreports.engine.design.JasperDesign;
-import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
 import net.sf.jasperreports.swing.JRViewer;
-import net.sf.jasperreports.view.JasperViewer;
 
 import javax.swing.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.sql.Connection;
 import java.util.HashMap;
 
@@ -22,23 +24,27 @@ public class FinalReportFormController {
     private Connection connection;
     private SwingNode swingNode;
 
-    public void initialize(){
+    public void initialize() {
         connection = DBConnection.getInstance().getConnection();
         swingNode = new SwingNode();
-        AnchorPane.setTopAnchor(swingNode,0.0);
-        AnchorPane.setBottomAnchor(swingNode,0.0);
-        AnchorPane.setLeftAnchor(swingNode,0.0);
-        AnchorPane.setRightAnchor(swingNode,0.0);
+        AnchorPane.setTopAnchor(swingNode, 0.0);
+        AnchorPane.setBottomAnchor(swingNode, 0.0);
+        AnchorPane.setLeftAnchor(swingNode, 0.0);
+        AnchorPane.setRightAnchor(swingNode, 0.0);
         pneContainer.getChildren().add(swingNode);
     }
 
     public void btnSearch_OnAction(ActionEvent actionEvent) {
         try {
-            JasperDesign jasperDesign = JRXmlLoader.load(this.getClass().getResourceAsStream("/report/final-report.jrxml"));
-            JasperDesign subJasperDesign = JRXmlLoader.load(this.getClass().getResourceAsStream("/report/sub-report.jrxml"));
+//            JasperDesign jasperDesign = JRXmlLoader.load(this.getClass().getResourceAsStream("/report/final-report.jrxml"));
+//            JasperDesign subJasperDesign = JRXmlLoader.load(this.getClass().getResourceAsStream("/report/sub-report.jrxml"));
 
-            JasperReport jasperReport = JasperCompileManager.compileReport(jasperDesign);
-            JasperReport subJasperReport = JasperCompileManager.compileReport(subJasperDesign);
+            //ObjectInputStream ois = new ObjectInputStream(this.getClass().getResourceAsStream("/report/final-report.jasper"));
+            //ObjectInputStream ois2 = new ObjectInputStream(this.getClass().getResourceAsStream("/report/sub-report.jasper"));
+            JasperReport jasperReport = (JasperReport)
+                    JRLoader.loadObject(this.getClass().getResource("/report/final-report.jasper")); //JasperCompileManager.compileReport(jasperDesign);
+            JasperReport subJasperReport = (JasperReport)
+                    JRLoader.loadObject(this.getClass().getResource("/report/sub-report.jasper")); //JasperCompileManager.compileReport(subJasperDesign);
 
             HashMap<String, Object> parameters = new HashMap<>();
             parameters.put("query", txtInput.getText());
@@ -48,13 +54,13 @@ public class FinalReportFormController {
             JRViewer viewer = new JRViewer(jasperPrint);
             viewer.setZoomRatio(0.75f);
 
-            SwingUtilities.invokeLater(()->{
+            SwingUtilities.invokeLater(() -> {
                 swingNode.setContent(viewer);
             });
 
             txtInput.requestFocus();
             txtInput.selectAll();
-        } catch (JRException e) {
+        } catch (JRException  e) {
             e.printStackTrace();
         }
     }
